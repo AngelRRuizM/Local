d90c9fdb7243517c7644305e0b090cb06ac98b8f
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-11-14 15:11:04 -0600

    Added missing fields (uva id, major, semester, etc.)

--- htdocs/insertaData.php
+++ htdocs/insertaData.php
@@ -9,6 +9,13 @@
 		$apellido2Error = null;
 		$nombre3Error = null;
 		$apellido3Error = null;
+		$semestre1Error = null;
+		$carrera1Error = null;
+		$semestre2Error = null;
+		$carrera2Error = null;
+		$semestre3Error = null;
+		$carrera3Error = null;
+		$uvaError = null;
 
 	if ( !empty($_POST)) {		
 		$nombre_equipo = $_POST['nombre_equipo'];
@@ -18,6 +25,13 @@
 		$apellido2 = $_POST['apellido2']; 
 		$nombre3 = $_POST['nombre3']; 
 		$apellido3 = $_POST['apellido3']; 
+		$semestre1 = $_POST['semestre1'];
+		$carrera1 = $_POST['carrera1'];
+		$semestre2 = $_POST['semestre2'];
+		$carrera2 = $_POST['carrera2'];
+		$semestre3 = $_POST['semestre3'];
+		$carrera3 = $_POST['carrera3'];
+		$uva = $_POST['uva'];
 		
 
 		$valid = true;
@@ -31,23 +45,51 @@
 		}
 
 		if (empty($nombre2)){
-			$apellidoError = 'Por favor escriba su nombre2';
+			$nombre2Error = 'Por favor escriba su nombre';
 			$valid = false;
 		}	
 		if (empty($apellido2)){
-			$nombre2Error = 'Por favor elija su apellido2';
+			$apellido2Error = 'Por favor elija su apellido';
 			$valid = false;
 		}		
 		if (empty($nombre3)){
-			$apellido2Error= 'Por favor elija a su nombre3 de apellido2';
+			$nombre3Error= 'Por favor elija a su nombre';
 			$valid = false;
 		}	
 		if (empty($apellido3)){
-			$nombre3Error= 'Por favor elija a su apellido3';
+			$apellido3Error= 'Por favor elija a su apellido';
 			$valid = false;
 		}	
 		if (empty($apellido)){
-			$apellido3Error= 'Por favor elija a su apellido';
+			$apellidoError= 'Por favor elija a su apellido';
+			$valid = false;
+		}
+		if (empty($semestre1)){
+			$semestre1Error= 'Por favor elija a su semestre';
+			$valid = false;
+		}
+		if (empty($carrera1)){
+			$carrera1Error= 'Por favor elija a su carrera';
+			$valid = false;
+		}
+		if (empty($semestre2)){
+			$semestre2Error= 'Por favor elija a su semestre';
+			$valid = false;
+		}
+		if (empty($carrera2)){
+			$carrera2Error= 'Por favor elija a su carrera';
+			$valid = false;
+		}
+		if (empty($semestre3)){
+			$semestre3Error= 'Por favor elija a su semestre';
+			$valid = false;
+		}
+		if (empty($carrera3)){
+			$carrera3Error= 'Por favor elija a su carrera';
+			$valid = false;
+		}
+		if (empty($uva)){
+			$uvaError= 'Por favor elija a su UVa ID';
 			$valid = false;
 		}
 								
@@ -56,10 +98,10 @@
 			
 			$pdo = Database::connect();
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "INSERT INTO datos (Nombre_equipo,Nombre,Apellido,Nombre2,Apellido2,Nombre3,Apellido3) values(?, ?, ?, ?, ?, ?, ? )";
+			$sql = "INSERT INTO datos (Nombre_equipo,Nombre,Apellido,Semestre1,Carrera1,Nombre2,Apellido2,Semestre2,Carrera2,Nombre3,Apellido3,Semestre3,Carrera3,Uva) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )";
 			$q = $pdo->prepare($sql);
 
-			$q->execute(array(addslashes($nombre_equipo),addslashes($nombre),addslashes($apellido),addslashes($nombre2),addslashes($apellido2),addslashes($nombre3),addslashes($apellido3)));
+			$q->execute(array(addslashes($nombre_equipo),addslashes($nombre),addslashes($apellido),addslashes($semestre1),addslashes($carrera1),addslashes($nombre2),addslashes($apellido2),addslashes($semestre2),addslashes($carrera2),addslashes($nombre3),addslashes($apellido3),addslashes($semestre3),addslashes($carrera3),addslashes($uva)));
 			Database::disconnect();
 			header("Location: salida.html");
 		}
